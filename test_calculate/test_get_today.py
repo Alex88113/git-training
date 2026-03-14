3351942f8d0c6e6d858ea2df3b7a9ebca9a3677f
Alex88113 <sanya.kucheryavvkh@bk.ru>
2026-03-15 00:21:27 +0300

    Update test_get_today.py

--- test_calculate/test_get_today.py
+++ test_calculate/test_get_today.py
@@ -3,9 +3,9 @@ import pytest
 from get_today import get_today_date
 
 @pytest.mark.parametrize('expected', [
-    ('2026-03-15'),
-    ('2026-03-15'),
-    ('2026-03-15')
+    ('2026-03-14'),
+    ('2026-03-14'),
+    ('2026-03-14')
 ])
 def test_get_date(expected):
     assert get_today_date() == expected
